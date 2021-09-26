b8a048a92f5269f8e2e0361208aa0de9a5fb53ba
schw3de <1119882+schw3de@users.noreply.github.com>
2021-09-26 19:12:00 +0200

    Added sample sprite.

---
+++ ludum-dare/Assets/Mover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace schw3de.ld
+{
+    public class Mover : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Translate(new Vector3(0.01f, 0, 0));
+        }
+    } 
+}
