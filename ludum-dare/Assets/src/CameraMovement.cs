fb3a558e216afe55f8b13ba968cb7019e8e86a50
schw3de <1119882+schw3de@users.noreply.github.com>
2022-04-02 14:19:17 +0200

    Web GL Build is working. Prototype cube is up.

---
+++ ludum-dare/Assets/src/CameraMovement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace schw3de.ld
+{
+    /// <summary>
+    /// Thx to for orignal code https://www.emmaprats.com/p/how-to-rotate-the-camera-around-an-object-in-unity3d/
+    /// </summary>
+    public class CameraMovement : MonoBehaviour
+    {
+        [SerializeField] private Camera cam;
+        [SerializeField] private Transform target;
+        [SerializeField] private float distanceToTarget = 10;
+
+        private Vector3 previousPosition;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                Vector3 newPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+                Vector3 direction = previousPosition - newPosition;
+
+                float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
+                float rotationAroundXAxis = direction.y * 180; // camera moves vertically
+
+                cam.transform.position = target.position;
+
+                cam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+                cam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World); // <— This is what makes it work!
+
+                cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+
+                previousPosition = newPosition;
+            }
+        }
+    }
+}
